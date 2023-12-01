ef83befcd1319fb50b15401ffee391ebefdd5a29
SiwyKot41 <frutkowski80@gmail.com>
2023-12-01 22:15:32 +0100

    refactor: add example of use nuxt ui component

--- pages/products.vue
+++ pages/products.vue
@@ -136,7 +136,8 @@ const suggestedProducts = [
           </div>
         </div>
         <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-          <p class="text-sm leading-6 text-gray-900" @click="isHidden = !isHidden">
+        <UButton label="Dodaj do koszyka" icon="i-heroicons-shopping-cart" color="black"/>
+        <!--  <p class="text-sm leading-6 text-gray-900" @click="isHidden = !isHidden">
             <button class="inline-flex items-center font-medium hover:underline">
               Dodaj do koszyka
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -145,7 +146,7 @@ const suggestedProducts = [
                   d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
               </svg>
             </button>
-          </p>
+        </p> -->
           <p class="mt-1 text-xs leading-5 text-gray-500">
             <a href="#">
               Sprawdź sugerowane
