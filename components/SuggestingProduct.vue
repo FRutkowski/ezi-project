534e0f0cc24272f8a0c868098da4891f57c800bc
monikaa1909 <monikaa1909@gmail.com>
2023-12-19 15:19:16 +0100

    feat: add components

---
+++ components/SuggestingProduct.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+const emit = defineEmits<{
+  (e: 'close-suggesting'): void,
+  (e: 'continue-shopping'): void
+}>()
+
+</script>
+
+<template>
+  <UModal prevent-close>
+      <div class="p-4">
+        <Placeholder class="h-48">
+          <div class="flex justify-between w-full py-4">
+            <p class="text-sm">
+              Inne często występujące razem produkty:
+            </p>
+            <UButton color="black" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
+              @click="$emit('close-suggesting')" />
+          </div>
+          <UDivider />
+          <slot></slot>
+          <UDivider />
+          <div class="pt-4 w-full flex justify-around">
+            <UButton color="gray" label="Kontynuuj zakupy" icon="i-heroicons-banknotes" @click="$emit('continue-shopping')" />
+            <UButton label="Przejdź do koszyka" icon="i-heroicons-shopping-cart" @click="$router.push('/shoppingCart'), $emit('continue-shopping')" />
+          </div>
+        </Placeholder>
+      </div>
+    </UModal>
+</template>
