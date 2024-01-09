9e9742bf9047f6e00ba4435198662910d10860ed
SiwyKot41 <frutkowski80@gmail.com>
2024-01-09 10:06:28 +0100

    feat: add composables to fetch the data from backend

--- types/models.d.ts
+++ types/models.d.ts
@@ -1,7 +1,22 @@
-interface Product {
+declare interface Product {
   id: number,
   name: string,
   price: number,
   category: string,
   photo: string
+}
+
+declare interface Order {
+  id: number
+  date: Date
+  quantity: number
+  price: number
+  products: Product[]
+}
+
+declare interface Client {
+  id: number
+  firstName: string
+  lastName: string
+  address: string
 }
