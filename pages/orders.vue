e0aadfa7212b5250ea58d2f6a4ad5fe495539957
SiwyKot41 <frutkowski80@gmail.com>
2024-01-09 09:40:56 +0100

    refactor: make the code more readable

--- pages/orders.vue
+++ pages/orders.vue
@@ -78,31 +78,33 @@ function select (row: any) {
 </script>
 
 <template>
-  <div class="text-3xl font-bold py-10">
-    Moje zamówienia
+  <div>
+    <div class="text-3xl font-bold py-10">
+      Moje zamówienia
+    </div>
+    <UCard :ui="{ body: { padding: '' } }">
+      <UTable
+        v-model:columns="columns"
+        :rows="orders"
+        @select="select"
+      >
+        <template #image-data="{ row }">
+          <UAvatarGroup
+            v-for="img in row.products"
+            class=""
+          >
+            <UTooltip :text="img.name">
+              <UAvatar
+                v-model:src="img.url"
+                class="flex"
+              />
+            </UTooltip>
+          </UAvatarGroup>
+        </template>
+        <template #amount-data="{ row }">
+          {{ row.amount }} zł
+        </template>
+      </UTable>
+    </UCard>
   </div>
-  <UCard :ui="{ body: { padding: '' } }">
-    <UTable
-      v-model:columns="columns"
-      :rows="orders"
-      @select="select"
-    >
-      <template #image-data="{ row }">
-        <UAvatarGroup
-          v-for="img in row.products"
-          class=""
-        >
-          <UTooltip :text="img.name">
-            <UAvatar
-              v-model:src="img.url"
-              class="flex"
-            />
-          </UTooltip>
-        </UAvatarGroup>
-      </template>
-      <template #amount-data="{ row }">
-        {{ row.amount }} zł
-      </template>
-    </UTable>
-  </UCard>
 </template>
